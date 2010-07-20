a428d1e5e796c0b7c91cf385d4f90df15e78b4aa
Jim Kang <jim.kang@modolabs.com>
2010-07-20 12:11:46 -0400

    Fixed bug in which 'title' showed up as the subtitle for recently searched entries in people search instead of the value of the title.

--- Modules/People/PeopleSearchViewController.m
+++ Modules/People/PeopleSearchViewController.m
@@ -348,7 +348,7 @@ NSInteger strLenSort(NSString *str1, NSString *str2, void *context)
 			NSArray *displayPriority = [NSArray arrayWithObjects:@"title", @"ou", nil];
 			NSString *displayText;
 			for (NSString *tag in displayPriority) {
-				if (displayText = [recent displayNameForKey:tag]) {
+				if (displayText = [recent actualValueForKey:tag]) {
 					cell.detailTextLabel.text = displayText;
 					break;
 				}
