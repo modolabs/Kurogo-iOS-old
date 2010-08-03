b9a3bc097802cc9da13574e75051982e7a1a5282
Jim Kang <jim.kang@modolabs.com>
2010-08-03 16:42:33 -0400

    Update people directory test for fixed title format.

--- tests/people_search_test.js
+++ tests/people_search_test.js
@@ -264,7 +264,7 @@ function testSuiteFieldsSeparatedByNewLine()
 {
 	var expectedSearchResultValues = {
 		"email": "jmurcian@law.harvard.edu",
-		"title": "Associate Director\nOPIA and Director of Fellowships\nTutor\nAssistant Sr (Harv Std)\nContin Ed/Spec Prog Instructor",
+		"title": "Associate Director, OPIA and Director of Fellowships\nTutor, Assistant Sr (Harv Std)\nContin Ed/Spec Prog Instructor",
 		"fax": "+1-617-496-4944",
 		"unit": "HLS / Pblc Interest\nFAS / FCOL / Leverett-Oth\nFAS / FDCE / Other Academic"
 	};
