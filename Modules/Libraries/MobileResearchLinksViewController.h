8ca5ff7978b1ae120b1b5814dad14bab4dc369e3
Muhammad J Amjad <jehangiramjad@Muhammad-J-Amjads-MacBook-Pro.local>
2010-12-17 12:58:04 -0500

    Library iPhone: first pass at Mob research links (tapping does not work yet)

---
+++ Modules/Libraries/MobileResearchLinksViewController.h
@@ -0,0 +1,19 @@
+//
+//  MobileResearchLinksViewController.h
+//  Harvard Mobile
+//
+//  Created by Muhammad J Amjad on 12/17/10.
+//  Copyright 2010 ModoLabs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MobileResearchLinksViewController : UITableViewController {
+	
+	NSDictionary * textAndLinksDictionary;
+	NSArray * textsArray;
+
+}
+
+@end
