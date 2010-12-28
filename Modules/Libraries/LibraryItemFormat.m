506f8008b878bb8007c698e83a22fe08eae4ca67
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2010-12-28 14:08:50 -0500

    Added new files to track new location and format feeds

---
+++ Modules/Libraries/LibraryItemFormat.m
@@ -0,0 +1,17 @@
+// 
+//  LibraryItemFormat.m
+//  Harvard Mobile
+//
+//  Created by Alexandra Ellwood on 12/27/10.
+//  Copyright 2010 ModoLabs Inc. All rights reserved.
+//
+
+#import "LibraryItemFormat.h"
+
+
+@implementation LibraryItemFormat 
+
+@dynamic name;
+@dynamic code;
+
+@end
---
+++ Modules/Libraries/LibraryLocation.h
@@ -0,0 +1,22 @@
+//
+//  LibraryLocation.h
+//  Harvard Mobile
+//
+//  Created by Alexandra Ellwood on 12/27/10.
+//  Copyright 2010 ModoLabs Inc. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface LibraryLocation :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * code;
+
+@end
+
+
+
