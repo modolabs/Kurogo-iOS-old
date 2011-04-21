c3a5ae56ee21273dbc344cfd90610249d2d5f05f
sonya huang <sonya.huang@modolabs.com>
2011-04-21 16:33:28 -0400

    use renamed functions from KGOWebViewControllerDelegate.

--- Modules/Login/ModalLoginWebViewController.m
+++ Modules/Login/ModalLoginWebViewController.m
@@ -59,8 +59,8 @@
     DLog(@"attempting to load %@ %@ %p", url, request, request);
     DLog(@"%@ %@", [url host], [[KGORequestManager sharedManager] host]);
     
-    if ([self.delegate respondsToSelector:@selector(webViewController:shouldLoadExternallyForURL:)]) {
-        if ([self.delegate webViewController:self shouldLoadExternallyForURL:url]) {
+    if ([self.delegate respondsToSelector:@selector(webViewController:shouldOpenSystemBrowserForURL:)]) {
+        if ([self.delegate webViewController:self shouldOpenSystemBrowserForURL:url]) {
             if ([[UIApplication sharedApplication] canOpenURL:url]) {
                 [[UIApplication sharedApplication] openURL:url];
             }
