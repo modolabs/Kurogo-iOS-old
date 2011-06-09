c1dbfb033262890e6ecf1cf94ca8203eb8d55259
sonya huang <sonya.huang@modolabs.com>
2011-06-08 21:17:38 -0400

    just learned about NSClassFromString, could have been helpful

--- Projects/Universitas/Application/KGOModule+Factory.m
+++ Projects/Universitas/Application/KGOModule+Factory.m
@@ -37,42 +37,17 @@
         NSString *serverID = [args objectForKey:@"id"];
         className = [moduleMap objectForKey:serverID];
     }
+
+    if (className) {
+        Class moduleClass = NSClassFromString(className);
+        if (moduleClass) {
+            module = [[[moduleClass alloc] initWithDictionary:args] autorelease];
+        }
+    }
     
-    if ([className isEqualToString:@"AboutModule"])
-        module = [[[AboutModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"ContentModule"])
-        module = [[[ContentModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"CalendarModule"])
-        module = [[[CalendarModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"HomeModule"])
-        module = [[[HomeModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"ExternalURLModule"])
-        module = [[[ExternalURLModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"EmergencyModule"])
-        module = [[[EmergencyModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"LoginModule"])
-        module = [[[LoginModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"MapModule"])
-        module = [[[MapModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"NewsModule"])
-        module = [[[NewsModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"PeopleModule"])
-        module = [[[PeopleModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"SettingsModule"])
-        module = [[[SettingsModule alloc] initWithDictionary:args] autorelease];
-    
-    else if ([className isEqualToString:@"VideoModule"])
-        module = [[[VideoModule alloc] initWithDictionary:args] autorelease];
+    if (!module) {
+        DLog(@"could not initialize module with params: %@", [args description]);
+    }
     
     return module;
 }
